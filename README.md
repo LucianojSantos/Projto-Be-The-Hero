75d59430c30c6da64621f091287b46ba5ebbacf4
unknown <luciano_arango@msn.com>
2020-04-01 22:00:06 -0300

    Be The Hero

---
+++ README.md
@@ -0,0 +1,1 @@
+"# be-the-hero"  git init git add README.md git commit -m "first commit" git remote add origin https://github.com/LucianojSantos/be-the-hero.git git push -u origin master 
---
+++ backend/src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*"
+  })
+);
+
+/**
+ * rota / recurso
+ */
+/**Metodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+/**
+ * Tipos de parâmetros
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" ( filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+ /**
+  * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+  * NoSQL: MongoDB, CouchDB, etc
+  */
+ /**
+  * Driver: SELECT * FROM users
+  * Query Builder: Table('users).select('*').where()npm
+  */
+ app.use(express.json());
+ app.use(routes);
+app.listen(3333);
